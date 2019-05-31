b10c1d9ed127ec2845a7a73243859291841592f9
myronliu347 <myronliu347@gmail.com>
2019-05-31 10:57:17 +0800

    feat: add message

--- src/utils.js
+++ src/utils.js
@@ -12,6 +12,31 @@ export const initDemo = (compName) => {
       compName: componentName,
     }, '*');
   }
+
+  window.addEventListener('message', ({origin, data}) => {
+    if (origin && origin.indexOf('fusion') !== -1) {
+      const { from = '', type = '' } = (data || {});
+      if (from !== 'fusion') return;
+      switch (type) {
+        case 'stylesheet':
+          const { stylesheets = []} = data;
+          document.querySelectorAll('[data-theme-style]').forEach(element => element.parentNode.removeChild(element));
+          stylesheets.forEach((url) => {
+            const stylesheet = document.createElement('link');
+            stylesheet.setAttribute('rel', 'stylesheet');
+            stylesheet.setAttribute('href', url);
+            stylesheet.setAttribute('data-theme-style', true);
+            document.head.appendChild(stylesheet);
+          });
+          return;
+        case 'change_background':
+          const { background = '' } = data;
+          document.body.style.background = background;
+          return;
+        default: break;
+      }
+    }
+  });
 };
 
 export const firstLowerCase = (str) => str.replace(/\B([A-Z])/g, "-$1").toLowerCase();
