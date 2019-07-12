79db0c161ae828e7cd049957c045921be2b677d7
myronliu347 <myronliu347@gmail.com>
2019-07-12 10:35:25 +0800

    fix: state

--- src/layouts/GenerateApp.js
+++ src/layouts/GenerateApp.js
@@ -21,8 +21,8 @@ export default (DemoView) => {
           const { shape, size, state, data } = props;
           const defaultProps = getDefaultProps(DemoView.Adaptor, shape);
           if (defaultProps.size && defaultProps.size !== size) return false;
-          if (defaultProps.state && defaultProps.state !== state) {
-            return false;
+          if (defaultProps.state) {
+            if (defaultProps.state !== state) return false;
           } else if (data && STATE_MARK[data.substring(0, 1)]) {
             return false;
           }
