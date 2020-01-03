786dfd7aca1d8f4afb9c71adbf80fed25234a146
myronliu347 <myronliu347@gmail.com>
2020-01-03 16:44:05 +0800

    fix:  增加 adaptor 规范校验

--- src/generate/pc/DemoItem.jsx
+++ src/generate/pc/DemoItem.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
 import PropTypes from 'prop-types';
 import { renderAdaptor } from '../../utils';
 
@@ -11,12 +12,32 @@ export default class DemoItem extends Component {
     adaptor: PropTypes.object,
   }
 
+  componentDidMount() {
+    const { adaptor } = this.props;
+    const dom = findDOMNode(this);
+    if (dom && dom.firstElementChild) {
+      const element = dom.firstElementChild;
+      if (!element.hasAttribute('data-fusioncool')) {
+        throw new Error(`[ERROR]: ${adaptor.name} 需要支持透传属性 [data-XXX]，否则 Fusion Cool 无法正常渲染`);
+      } else if (element.style.direction !== 'ltr') {
+        throw new Error(`[ERROR]: ${adaptor.name} 需要支持透传属性 [style]，否则 Fusion Cool 无法正常渲染`);
+        element.style.direction = '';
+      }
+    } else {
+      throw new Error(`[ERROR]: ${adaptor.name} DOM 节点不能使用 Portal挂载到其他地方奥`);
+    }
+  }
+
   render() {
     const { id, height, background, node, adaptor } = this.props;
 
     if (!node) return null;
     const demoContent = renderAdaptor(adaptor, {
       ...node.props,
+      style: {
+        direction: 'ltr'
+      },
+      'data-fusioncool': 'test fusion cool render',
     });
 
     return (
